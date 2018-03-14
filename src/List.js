5f6a3f6ace6669e591a31ec4408b68d4ad2f8caa
NoobTW <noob@noob.tw>
2018-03-15 00:25:24 +0800

    fix li style

--- src/List.js
+++ src/List.js
@@ -16,7 +16,7 @@ class List extends Component {
 				<h1 className="App-title">地方政府開放資料平台局處單位數量呈現</h1>
 			</header>
 			<h2>請選擇資料</h2>
-			<div className="datasets">
+			<div className="datasets list">
 				<ul>
 					<li><Link className="list-link" to={`${process.env.PUBLIC_URL}/treemap/2017/3`}><FontAwesomeIcon icon="briefcase" /> 2017-03（以局處分類）</Link></li>
 					<li><Link className="list-link" to={`${process.env.PUBLIC_URL}/board/2017/3`}><FontAwesomeIcon icon="fire" /> 2017-03（以關鍵字排行）</Link></li>
